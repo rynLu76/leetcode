389d0db6691afd2f257a781cfdab40db7e8c8b5f
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-25 19:34:21 -0500

    Create GroupAnagrams.py

---
+++ Array_and_String/GroupAnagrams.py
@@ -0,0 +1,7 @@
+class Solution(object):
+  def groupAnagrams(self, strs):
+    groups = collections.defaultdict(list)
+    for s in strs:
+      groups[tuple(sorted(s))].append(s)
+      
+    return map(sorted, groups.values())

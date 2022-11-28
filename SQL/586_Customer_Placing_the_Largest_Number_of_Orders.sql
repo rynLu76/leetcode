e46c9ba7c6150bb7d90a6539a3c86e40f77128aa
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-27 23:22:55 -0500

    Create 586_Customer_Placing_the_Largest_Number_of_Orders.sql

---
+++ SQL/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -0,0 +1,41 @@
+Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+
+
+
+----------------------------------------------------------------------
+select customer_number
+from orders
+group by customer_number
+order by count(*)
+limit 1;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+

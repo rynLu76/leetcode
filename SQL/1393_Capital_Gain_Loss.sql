65b0afed055cba2def0bccc99400d7d5ed4b53ea
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-28 11:39:29 -0500

    Create 1393_Capital_Gain_Loss.sql

---
+++ SQL/1393_Capital_Gain_Loss.sql
@@ -0,0 +1,58 @@
+Write an SQL query to report the Capital gain/loss for each stock.
+
+The Capital gain/loss of a stock is the total gain or loss after buying and selling the stock one or many times.
+
+
+
+Table: Stocks
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| stock_name    | varchar |
+| operation     | enum    |
+| operation_day | int     |
+| price         | int     |
++---------------+---------+
+(stock_name, operation_day) is the primary key for this table.
+
+
+
+----------------------------------------------------------------------
+select stock_name,
+sum(
+  case 
+    when operation = "buy" then -price
+    else price
+  end
+) as capital_gain_loss
+from stocks
+group by stock_name;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Stocks table:
++---------------+-----------+---------------+--------+
+| stock_name    | operation | operation_day | price  |
++---------------+-----------+---------------+--------+
+| Leetcode      | Buy       | 1             | 1000   |
+| Corona Masks  | Buy       | 2             | 10     |
+| Leetcode      | Sell      | 5             | 9000   |
+| Handbags      | Buy       | 17            | 30000  |
+| Corona Masks  | Sell      | 3             | 1010   |
+| Corona Masks  | Buy       | 4             | 1000   |
+| Corona Masks  | Sell      | 5             | 500    |
+| Corona Masks  | Buy       | 6             | 1000   |
+| Handbags      | Sell      | 29            | 7000   |
+| Corona Masks  | Sell      | 10            | 10000  |
++---------------+-----------+---------------+--------+
+Output: 
++---------------+-------------------+
+| stock_name    | capital_gain_loss |
++---------------+-------------------+
+| Corona Masks  | 9500              |
+| Leetcode      | 8000              |
+| Handbags      | -23000            |
++---------------+-------------------+

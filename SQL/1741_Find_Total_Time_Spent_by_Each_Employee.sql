3cd04928dc755311432e3efadaf2cbd6bf25a875
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-28 11:32:05 -0500

    Create 1741_Find_Total_Time_Spent_by_Each_Employee.sql

---
+++ SQL/1741_Find_Total_Time_Spent_by_Each_Employee.sql
@@ -0,0 +1,47 @@
+Write an SQL query to calculate the total time in minutes spent by each employee on each day at the office. Note that within one day, an employee can enter and leave more than once. The time spent in the office for a single entry is out_time - in_time.
+
+
+
+Table: Employees
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| emp_id      | int  |
+| event_day   | date |
+| in_time     | int  |
+| out_time    | int  |
++-------------+------+
+(emp_id, event_day, in_time) is the primary key of this table.
+The table shows the employees' entries and exits in an office.
+
+
+
+----------------------------------------------------------------------
+select event_day as day, emp_id, sum(out_time-in_time) as total_time
+from employees
+group by 1,2;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Employees table:
++--------+------------+---------+----------+
+| emp_id | event_day  | in_time | out_time |
++--------+------------+---------+----------+
+| 1      | 2020-11-28 | 4       | 32       |
+| 1      | 2020-11-28 | 55      | 200      |
+| 1      | 2020-12-03 | 1       | 42       |
+| 2      | 2020-11-28 | 3       | 33       |
+| 2      | 2020-12-09 | 47      | 74       |
++--------+------------+---------+----------+
+Output: 
++------------+--------+------------+
+| day        | emp_id | total_time |
++------------+--------+------------+
+| 2020-11-28 | 1      | 173        |
+| 2020-11-28 | 2      | 30         |
+| 2020-12-03 | 1      | 41         |
+| 2020-12-09 | 2      | 27         |
++------------+--------+------------+

7a1960138b22a0ad3a0d36ab379b19dfc343a078
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-29 23:42:23 -0500

    Create 184_Department_Highest_Salary.sql

---
+++ SQL/184_Department_Highest_Salary.sql
@@ -0,0 +1,72 @@
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table.
+departmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write an SQL query to find employees who have the highest salary in each of the departments.
+
+
+
+----------------------------------------------------------------------
+SELECT Department.name AS Department ,Employee.name AS Employee, Employee.salary
+FROM Department 
+JOIN Employee 
+ON Employee.departmentId=Department.id 
+WHERE(departmentId, salary) IN
+(
+  select departmentId, max(salary)
+  from Employee
+  group by departmentId
+);
+----------------------------------------------------------------------
+
+
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
+| IT         | Max      | 90000  |
++------------+----------+--------+

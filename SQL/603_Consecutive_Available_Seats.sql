a903059e22a37486970e8b3bc321147afcc19266
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-29 22:50:24 -0500

    Create 603_Consecutive_Available_Seats.sql

---
+++ SQL/603_Consecutive_Available_Seats.sql
@@ -0,0 +1,49 @@
+Table: Cinema
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| seat_id     | int  |
+| free        | bool |
++-------------+------+
+seat_id is an auto-increment primary key column for this table.
+Each row of this table indicates whether the ith seat is free or not. 1 means free while 0 means occupied.
+ 
+
+Write an SQL query to report all the consecutive available seats in the cinema.
+
+Return the result table ordered by seat_id in ascending order.
+
+
+
+----------------------------------------------------------------------
+select distinct a.seat_id
+from Cinema a
+join Cinema b
+on abs(a.seat_id - b.seat_id) = 1
+and a.free = 1
+and b.free = 1
+order by a.seat_id;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Cinema table:
++---------+------+
+| seat_id | free |
++---------+------+
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
++---------+------+
+Output: 
++---------+
+| seat_id |
++---------+
+| 3       |
+| 4       |
+| 5       |
++---------+

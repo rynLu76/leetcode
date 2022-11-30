80d667b12cfd032576e4c1e550298a8a369242e5
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-11-29 23:37:14 -0500

    Create 180_Consecutive_Numbers.sql

---
+++ SQL/180_Consecutive_Numbers.sql
@@ -0,0 +1,46 @@
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+
+
+----------------------------------------------------------------------
+select distinct l1.num as ConsecutiveNums
+from logs l1, logs l2, logs l3
+where l1.id = l2.id-1
+and l2.id = l3.id-1
+and l1.num = l2.num
+and l2.num = l3.num;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+

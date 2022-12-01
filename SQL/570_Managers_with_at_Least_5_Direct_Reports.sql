37ca45ed88ce2ba29602e80a2cfecdd99c481f05
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-12-01 00:17:57 -0500

    Create 570_Managers_with_at_Least_5_Direct_Reports.sql

---
+++ SQL/570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,49 @@
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write an SQL query to report the managers with at least five direct reports.
+
+
+
+----------------------------------------------------------------------
+select e2.name
+from employee e1
+join employee e2
+on e1.manageId = e2.id
+group by e1.managerId
+having count(e1.managerId) >=5;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+

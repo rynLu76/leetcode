2160c0d6a8d4a86cf15282c5aaf43cb34b804def
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-12-01 00:08:57 -0500

    Update 550_Game_Play_Analysis_IV.sql

--- SQL/550_Game_Play_Analysis_IV.sql
+++ SQL/550_Game_Play_Analysis_IV.sql
@@ -17,4 +17,34 @@ Write an SQL query to report the fraction of players that logged in again on the
 
 
 
-----------
+----------------------------------------------------------------------
+select round(count(a2.player_id)/count(a1.player_id), 2) as fraction
+(
+ select player_id, min(event_date) as first_login
+ from activity
+ group by player_id
+) a1
+left join activity a2
+on a1.player_id = a2.player_id
+and a1.first_login = a2.event_date-1;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+
+| fraction  |
++-----------+
+| 0.33      |
++-----------+

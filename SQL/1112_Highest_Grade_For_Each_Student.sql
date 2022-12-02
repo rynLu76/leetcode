43fecc65375c8d2475962fee155c2495a6494c1e
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-12-01 21:13:55 -0500

    Create 1112_Highest_Grade_For_Each_Student.sql

---
+++ SQL/1112_Highest_Grade_For_Each_Student.sql
@@ -0,0 +1,52 @@
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key of this table.
+ 
+
+Write a SQL query to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+
+
+----------------------------------------------------------------------
+select distinct student_id, min(course_id) as course_id, grade
+from enrollments
+where (student_id, grade) in
+(
+  select distinct student_id, max(grade)
+  from enrollments
+  group by student_id
+)
+group by student_id
+order by student_id;
+----------------------------------------------------------------------
+
+
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+

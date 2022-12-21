87bbd7cf7a2c915fa92d4b80980d53b9c6fe5160
rynLu76 <43496806+rynLu76@users.noreply.github.com>
2022-12-21 17:08:47 -0500

    Create 139_Word_Break.py

---
+++ DynamicProgramming/139_Word_Break.py
@@ -0,0 +1,47 @@
+'''
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+'''
+
+
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+
+        '''
+        The idea is the following:
+
+        d is an array that contains booleans
+
+        d[i] is True if there is a word in the dictionary that ends at ith index of s AND d is also True at the beginning of the word
+        '''
+        d = [False] * len(s)    
+        for i in range(len(s)):
+            for w in wordDict:
+                if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+                    d[i] = True
+        return d[-1]
